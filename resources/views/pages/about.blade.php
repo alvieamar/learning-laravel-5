ed6df3d75a9e6615dfcdb1b01b86c100ae7c8516
alvieamar <alvie.amar@bibo.com.ph>
2016-11-16 17:49:36 +0800

    modified view/pages for blace implementation

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,10 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>About Page</title>
-</head>	
-<body>
-	<h1>About Me: {{ $first }} {{ $last }}</h1>
-	<p>Lorem ipsum dolor sit amet, semper instructior usu et, mei ad noster repudiandae. Soluta recusabo argumentum id qui, zril epicurei per eu. Ne per unum illum. In expetendis dissentiet per, aeque ubique pericula per ne. Vix ad etiam maluisset, modus laudem putent eam ex.</p>
-</body>
-</html 
+@extends('app')
+
+@section('content')
+
+@if ($first == 'Fox1')
+	<h1>Hi Fox</h1>
+@else
+	<h1>Else</h1>
+@endif
+
+<h1>About Me: {{ $first }} {{ $last }}</h1>
+
+@if (count($people))
+	<h3>People I like: </h3>
+	<ul>
+		@foreach ($people as $person)
+			<li>{{ $person }}</li>
+		@endforeach
+	</ul>
+@endif
+
+<p>Lorem ipsum dolor sit amet, semper instructior usu et, mei ad noster repudiandae. Soluta recusabo argumentum id qui, zril epicurei per eu. Ne per unum illum. In expetendis dissentiet per, aeque ubique pericula per ne. Vix ad etiam maluisset, modus laudem putent eam ex.</p>
+
+@stop
