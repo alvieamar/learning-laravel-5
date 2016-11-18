a4047e9bd7fea95db042cb767c70eed382758798
alvieamar <alvie.amar@bibo.com.ph>
2016-11-18 18:54:59 +0800

    added route for ArticlesController@create@store

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,25 +5,35 @@ namespace App\Http\Controllers;
 use App\Article;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Request;
 
 class ArticlesController extends Controller
 {
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::latest('published_at')->get();
 
         return view('articles.index', compact('articles'));
     }
 
     public function show($id)
     {
-        $article = Article::find($id);
-
-        if (is_null($article)) {
-            abort(404);
-        }
+        $article = Article::findOrFail($id);
 
         return view('articles.show', compact('article'));
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store()
+    {
+        $input = Request::all();
+        $input['published_at'] = Carbon::now();
+        Article::create($input);
+        return redirect('articles');
     }
 }
