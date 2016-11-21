f88878b2f28cd83a7f40e6f354a397b4e6da81f5
alvieamar <alvie.amar@bibo.com.ph>
2016-11-21 15:36:25 +0800

    added error notification on form

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -4,6 +4,13 @@
     <h1>Write a new Article</h1>
 
     <hr/>
+    @if ($errors->any())
+        <ul class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
 
     {!! Form::open(['url' => 'articles']) !!}
         <div class="form-group">
